2ffbbc4a073cc67ef9616c6115ebfc0f23159d37
Arush2411 <86655897+Arush2411@users.noreply.github.com>
2021-09-30 16:44:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(400,400);
+    canvas.center();
+}
+
+
+function draw()
+{
+
+}
+
+function pic_click()
+{
+    save('Filter_Imae.png');
+}
+
+
